44c780119106692ec0cfe4cb1ca5211ab1006af2
Pia Baronetzky <pia@baronetzky.org>
2023-11-24 16:11:34 +0100

    Adapted the code for creating PD to anesthesia data

--- Anesthesia_Data/Time_Series/Converting_Data.m
+++ Anesthesia_Data/Time_Series/Converting_Data.m
@@ -1,5 +1,6 @@
+label = 4
 
-time_series_data = load('./Data/m292/trimm_eeg.mat');
+time_series_data = load('./Data/m292/run0'+string(label)+'/trimm_eeg.mat');
 
 time_series_data = rmfield(time_series_data,"finish")
 time_series_data = rmfield(time_series_data,"start")
@@ -8,5 +9,5 @@ time_series_data = rmfield(time_series_data,"trig")
 
 %csvwrite('Anesthesia_Time_Series_Data.csv', time_series_data);
 
-writetable(struct2table(time_series_data), './Data/m292/Time_Series_Data.csv')
+writetable(struct2table(time_series_data), './Data/m292/run0'+string(label)+'/Time_Series_Data.csv')
 
